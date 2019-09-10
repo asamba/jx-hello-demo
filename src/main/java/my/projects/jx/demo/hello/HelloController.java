7e1fbea931b389b35d1204e6c0138c08ec8d1fa1
asamba <anand.sambamoorthy@gmail.com>
2019-09-10 12:58:17 -0700

    hellocontroller from feature branch

--- src/main/java/my/projects/jx/demo/hello/HelloController.java
+++ src/main/java/my/projects/jx/demo/hello/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World from feature-branch!";
     }
 }
