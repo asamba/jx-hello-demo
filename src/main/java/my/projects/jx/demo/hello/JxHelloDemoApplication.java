b270f599a66231e416401030d73ce66796f29bb3
asamba <anand.sambamoorthy@gmail.com>
2019-09-10 12:35:33 -0700

    Initial commit

---
+++ src/main/java/my/projects/jx/demo/hello/JxHelloDemoApplication.java
@@ -0,0 +1,13 @@
+package my.projects.jx.demo.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxHelloDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxHelloDemoApplication.class, args);
+	}
+
+}
