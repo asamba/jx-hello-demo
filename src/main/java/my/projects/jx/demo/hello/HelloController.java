c7f91beda1a75662831f9537526651c663b15d4a
asamba <anand.sambamoorthy@gmail.com>
2019-09-10 12:42:40 -0700

    hellocontroller

---
+++ src/main/java/my/projects/jx/demo/hello/HelloController.java
@@ -0,0 +1,17 @@
+package my.projects.jx.demo.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by asamba on 9/10/19.
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
